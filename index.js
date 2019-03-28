0f8527778751548498689bdf5bf82c731a1263d8
Alex Odorico <alex.odorico@morleynet.com>
2019-03-28 12:44:28 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+$(function() {
+	let $categoryItems = document.getElementsByClassName('category-item');
+	let $selectedCategoriesList = document.getElementById('selected-categories');
+
+	for (let element of $categoryItems) {
+		element.addEventListener("click", function(event) {
+			addCategoryToView(event);
+		});
+	}
+
+	function addCategoryToView(event) {
+		let alreadySelected = toggleDataSelected(event);
+		if (alreadySelected) return;
+		let categoryName = getCategoryName(event);
+		let categoryElement = createCategoryElement(categoryName);
+		appendCategoryElement(categoryElement);
+		let $selectedCategoryItems = document.getElementsByClassName('selected-category-item');
+		addEventListener($selectedCategoryItems);
+	}
+
+	function getCategoryName(event) {
+		let categoryName = event.target.innerText;
+		return categoryName;
+	}
+
+	function createCategoryElement(categoryName) {
+		let categoryElement = document.createElement('li');
+		categoryElement.innerHTML = categoryName;
+		categoryElement.className = "selected-category-item";
+		return categoryElement;
+	}
+
+	function appendCategoryElement(categoryElement) {
+		$selectedCategoriesList.append(categoryElement);
+	}
+
+	function addEventListener($elements) {
+		for (let element of $elements) {
+			element.addEventListener("click", function(event) {
+				removeCategoryFromView(event);
+			});
+		}
+	}
+
+	function removeCategoryFromView(event) {
+		let categoryName = event.target.innerText;
+		for (let element of $categoryItems) {
+			if (element.innerText == categoryName) element.dataset.selected = false;
+		}
+		event.target.remove();
+	}
+
+	function toggleDataSelected(event) {
+		let dataset = event.target.parentElement.dataset;
+		if (dataset.selected === "false") {
+			dataset.selected = "true";
+			return false;
+		} else {
+			return true;
+		}
+	}
+});
