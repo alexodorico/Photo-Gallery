373b27c27bb2dea8af39fb19e5d8719bb2dc6bcd
Alex Odorico <alex.odorico@morleynet.com>
2019-04-08 15:53:22 -0400

    Can now switch between selected photos and all photos

--- index.js
+++ index.js
@@ -7,30 +7,42 @@ $(function() {
 	var API_QUERY = 'assets/search?query_category=Morley+Asset%2FPhotography&limit=30&offset=' + offset;
 	var ApiCall = API_BASE + API_QUERY;
 	var selectedPhotoLinks = [];
-	var selectedPhotoMarkup = '';
+	var selectedPhotoElement = [];
+	var previousView = '';
 
 	generateImages(ApiCall);
 
-	var viewingSelected = true;
+	// todo: get event listeners to persist
 	$('#view-selected-button').click(function() {
-		generateSelectedPhotoMarkup();
-		// View Selected Photos
-		// Change button text to view all
-	})
+		if (this.innerText === "View Selected") {
+			this.innerText = "View All";
+			getPreviousView();
+			$('#photo-grid')[0].innerHTML = generateSelectedPhotoMarkup();
+		} else {
+			this.innerText = "View Selected";
+			$('#photo-grid')[0].innerHTML = previousView;
+		}
+	});
+
+	function getPreviousView() {
+		previousView = $('#photo-grid')[0].innerHTML;
+	}
 
 	function generateSelectedPhotoMarkup() {
-		// use data in selectedPhotoLinks array
-		// use markup in generateImages()
+		var newView = '';
+		selectedPhotoElement.forEach(function(element) {
+			newView += element.outerHTML;
+		});
+		return newView;
 	}
 
 	function generateImages(ApiCall) {
 		$.get(ApiCall, function(data) {
-			console.log(data);
-			var content = '';
+			var initialContent = '';
 
 			for (var i = 0; i < data.items.length; i++) {
 				// switch to regular quotes when ready for production
-				content += `
+				initialContent += `
 				<div class="item" id="${data.items[i].id}" downloadLink="${data.items[i]._links.download}">
 					<img src="https://via.placeholder.com/300x200"></img>
 					<div class="overlay">
@@ -47,7 +59,7 @@ $(function() {
 				</div>`
 			}
 
-			$('#photo-grid').append(content);
+			$('#photo-grid').append(initialContent);
 			$('.select-button').on('click', handleSelectButtonClick);
 			$('.download-button').on('click', handleSingleDownloadClick);
 		});
@@ -95,18 +107,17 @@ $(function() {
 			// Has this class when selected AND hovered over
 			if ($(this).hasClass('btn-danger')) {
 				var $this = $(this);
+				deselectPhoto($this);
 				$(this)
 					.off('mouseover')
 					.removeClass('btn-danger')
 					.addClass('btn-default')
 					.html(unselectedButtonText);
-	
-				deselectPhoto($this);
 			}
 		}
 	}
 
-	function handleSingleDownloadClick(e) {
+	function handleSingleDownloadClick() {
 		var downloadLink = this.dataset.link;
 		$('iframe').attr("src", downloadLink);
 	}
@@ -120,82 +131,19 @@ $(function() {
 
 	function selectPhoto($this) {
 		var downloadLink = $this.parent().parent().parent().attr('downloadLink');
+		var photoMarkup = $this.parent().parent().parent()[0];
 		selectedPhotoLinks.push(downloadLink);
+		selectedPhotoElement.push(photoMarkup);
 	}
 
 	function deselectPhoto($this) {
 		var downloadLink = $this.parent().parent().parent().attr('downloadLink');
-		var index = selectedPhotoLinks.indexOf(downloadLink);
-		selectedPhotoLinks.splice(index, 1);
+		var downloadLinkIndex = selectedPhotoLinks.indexOf(downloadLink);
+		var photoElement = $this.parent().parent().parent()[0];
+		var photoElementIndex = selectedPhotoElement.indexOf(photoElement);
+		selectedPhotoLinks.splice(downloadLinkIndex, 1);
+		selectedPhotoElement.splice(photoElementIndex, 1);
 	}
-
-	//	TO TEST WITH REAL IMAGES FOR STYLING
-	
-	// async function generateImages(ApiCall) {
-	// 	let response = await fetch(ApiCall);
-	// 	let data = await response.json();
-	// 	data = getLinks(data);
-	// 	getImageData(data);
-	// }
-	
-	// function getLinks(response) {
-	// 	let rawLinks = [];
-	// 	let parsedLinks = [];
-	// 	let itemsArray = response.items;
-
-	// 	itemsArray.forEach(item => {
-	// 		rawLinks.push(item._links.self_all);
-	// 	});
-
-	// 	rawLinks.forEach(link => {
-	// 		parsedLinks.push(parseLink(link, 3));
-	// 	})
-
-	// 	return parsedLinks;
-	// }
-
-	// function parseLink(path, start) {
-	// 	let indices = [];
-	// 	for (let i = 0; i <path.length; i++) {
-	// 		if (path[i] === '/') indices.push(i);
-	// 	}
-	// 	return path.slice(indices[start] + 1);
-	// }
-
-	// function getImageData(imageData) {
-	// 	imageData.forEach(async function(link) {
-	// 		let response = await fetch(API_BASE + link);
-	// 		let data = await response.json();
-	// 		appendImage(data);
-	// 	});
-		
-	// }
-
-	// function appendImage(response) {
-	// 	let element = document.createElement('div');
-	// 	let photoGrid = document.getElementById('photo-grid');
-
-	// 	element.innerHTML = `
-	// 	<img src="${response.thumbnails['300px'].url}"></img>
-	// 	<div class="overlay">
-	// 		<h1>Title</h1>
-	// 		<div class="photo-controls">
-	// 			<button type="button" class="btn btn-default select-button">
-	// 				Select <span class="glyphicon glyphicon-plus-sign"></span>
-	// 			</button>
-	// 			<button type="button" class="btn btn-default download-button">
-	// 				<span class="glyphicon glyphicon-download-alt"></span>
-	// 			</button>
-	// 		</div>
-	// 	</div>`;
-
-	// 	element.className = 'item';
-	// 	element.id = response.id;
-
-	// 	photoGrid.append(element);
-
-	// 	$('#' + response.id + ' .select-button').on('click', handleSelectButtonClick);
-	// }
 
 	////////////////////////////////////////////////
 	// HANDLES ADDING/REMOVING CATEGORIES TO VIEW //
