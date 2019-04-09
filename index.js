31e5ebd81f14e96868adcd1ef251486ca3840fc2
Alex Odorico <alex.odorico@morleynet.com>
2019-04-09 12:01:30 -0400

    Moved things around

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 $(function() {
-	////////////////////////////////////////////////
-	// Fetch Photos from DAM API for initial view //
-	////////////////////////////////////////////////
 	var API_BASE = 'https://morleynet.morleycms.com/components/handlers/DamApiHandler.ashx?request=';
 	var offset = Math.floor(Math.random() * 4000).toString();
 	var API_QUERY = 'assets/search?query_category=Morley+Asset%2FPhotography&limit=30&offset=' + offset;
 	var ApiCall = API_BASE + API_QUERY;
-
 	var selectedButtonText = 'Selected <span class="glyphicon glyphicon-ok-circle"></span>';
 	var unselectedButtonText = 'Select <span class="glyphicon glyphicon-plus-sign"></span>';
 	var removeButtonText = 'Remove <span class="glyphicon glyphicon-minus-sign"></span>';
-
 	var selectedPhotoLinks = [];
 	var selectedPhotoElement = [];
 	var previousView = [];
-
 	var viewingSelected = false;
 
 	generateImages(ApiCall);
+
+	$('#view-selected-button').click(function() {
+		if (this.innerText === "View Selected") {
+			viewingSelected = true;
+			this.innerText = "View All";
+			getPreviousView();
+			$('#photo-grid')[0].innerHTML = generateMarkup(selectedPhotoElement);
+		} else {
+			viewingSelected = false;
+			this.innerText = "View Selected";
+			$('#photo-grid')[0].innerHTML = generateMarkup(previousView);
+		}
+
+		$('.select-button').on('click', handleSelectButtonClick);
+		reapplyHoverListeners();
+		$('.download-button').on('click', handleSingleDownloadClick);
+	});
 
 	function generateImages(ApiCall) {
 		$.get(ApiCall, function(data) {
 			var initialContent = '';
 
 			for (var i = 0; i < data.items.length; i++) {
-				// switch to regular quotes when ready for production
 				initialContent += `
 				<div class="item" id="${data.items[i].id}" downloadLink="${data.items[i]._links.download}">
 					<img src="https://via.placeholder.com/300x200"></img>
@@ -141,32 +151,17 @@ $(function() {
 		selectedPhotoElement.splice(photoElementIndex, 1);
 	}
 
+	// This isn't IE9 compatible, change later...
 	function updatePreviousView(photoElement) {
 		for (var i = 0; i < previousView.length; i++) {
 			if (photoElement.id === previousView[i].id) {
-				previousView[i].lastElementChild.lastElementChild.firstElementChild.classList.remove('btn-success');
-				previousView[i].lastElementChild.lastElementChild.firstElementChild.classList.add('btn-default');
-				previousView[i].lastElementChild.lastElementChild.firstElementChild.innerHTML = unselectedButtonText;
+				var previousViewItem = previousView[i].lastElementChild.lastElementChild.firstElementChild;
+				previousViewItem.classList.remove('btn-success');
+				previousViewItem.classList.add('btn-default');
+				previousViewItem.innerHTML = unselectedButtonText;
 			}
 		}
 	}
-
-	$('#view-selected-button').click(function() {
-		if (this.innerText === "View Selected") {
-			viewingSelected = true;
-			this.innerText = "View All";
-			getPreviousView();
-			$('#photo-grid')[0].innerHTML = generateMarkup(selectedPhotoElement);
-		} else {
-			viewingSelected = false;
-			this.innerText = "View Selected";
-			$('#photo-grid')[0].innerHTML = generateMarkup(previousView);
-		}
-
-		$('.select-button').on('click', handleSelectButtonClick);
-		reapplyHoverListeners();
-		$('.download-button').on('click', handleSingleDownloadClick);
-	});
 
 	function getPreviousView() {
 		previousView = $('.item').get();
