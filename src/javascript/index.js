7c97a8c11aa957b79067ccc81d433069ead25771
Alex Odorico <alex.odorico@morleynet.com>
2019-06-27 14:52:31 -0400

    API call works

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,10 +1,12 @@
 import '../scss/styles.scss';
+import options from '../../gallery.config';
 
-(_=> {
+(async _=> {
   const categories = window.categories || [ "Gala", "Fireworks", "Team-Building Event", "GM Topiary", "SOY Awards" ];
-  getById("category-dropdown-button").innerHTML = `${categories[0]} <span class="caret"></span>`;
+  updateCategoryDropdown(categories[0]);
   populateCategoriesDropDown(categories);
   addListenerToClass("category-name", "click", handleCategoryClick);
+  await fetchData(categories[0]);
 })();
 
 function populateCategoriesDropDown(categories) {
@@ -18,11 +20,33 @@ function populateCategoriesDropDown(categories) {
 }
 
 function handleCategoryClick(event) {
-  const categoryName = event.target.innerHTML;
+  const category = event.target.innerHTML;
 
-  getById("category-dropdown-button").innerHTML = `${categoryName} <span class="caret"></span>`;
+  updateCategoryDropdown(category);
 
   return true;
+}
+
+function updateCategoryDropdown(category) {
+  return getById("category-dropdown-button").innerHTML = `${category} <span class="caret"></span>`;
+}
+
+function fetchData(category = window.categories[0], offset = 0) {
+  const endpoint = buildAPICall(category, offset);
+
+  try {
+    return fetch(endpoint)
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => showError(err))
+  }
+  catch {
+    showError("Something went wrong while getting photo data");
+  }
+}
+
+function buildAPICall(category, offset) {
+  return `${options.endpoint}job=${window.jobNumber || "GT0000" }&cat=${category}&limit=${options.photoLimit.toString()}&offset=${offset.toString()}`;
 }
 
 
@@ -44,11 +68,11 @@ function getById(id) {
 }
 
 function scrollToTop() {
-  document.body.scrollTop = document.documentElement.scrollTop = 0;
+  return document.body.scrollTop = document.documentElement.scrollTop = 0;
 }
 
 function destroyHTML(id) {
-  getById(id).innerHTML = new String();
+  return getById(id).innerHTML = new String();
 }
 
 function getFromStorage(key) {
@@ -67,4 +91,8 @@ function addListenerToClass(className, event, handler) {
   });
 
   return true;
+}
+
+function showError(error) {
+  return console.log(error);
 }
