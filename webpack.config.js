fe9cd2c4ac3a37033497130fc17b819f304c773e
Alex Odorico <alex.odorico@morleynet.com>
2019-06-27 15:41:28 -0400

    Installed polyfill for async functions

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const cssExtract = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/javascript/index.js',
+  entry: ['babel-polyfill', './src/javascript/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js'
