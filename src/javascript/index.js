dcee1804ab3530785ac8ef41f4faf9a3899a05e7
Alex Odorico <alex.odorico@morleynet.com>
2019-06-27 14:23:43 -0400

    Have category dropdown working, made some helper functions

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,43 @@
-// import './zip';
 import '../scss/styles.scss';
+
+(_=> {
+  const categories = window.categories || [ "Gala", "Fireworks", "Team-Building Event", "GM Topiary", "SOY Awards" ];
+  getById("category-dropdown-button").innerHTML = `${categories[0]} <span class="caret"></span>`;
+  populateCategoriesDropDown(categories);
+  addListenerToClass("category-name", "click", handleCategoryClick);
+})();
+
+function populateCategoriesDropDown(categories) {
+  let markup = new String();
+
+  categories.forEach(category => {
+    markup += `<li class="category-item"><a class="category-name">${category}</a></li>`;
+  });
+
+  return getById("category-list").innerHTML = markup;
+}
+
+function handleCategoryClick(event) {
+  const categoryName = event.target.innerHTML;
+
+  getById("category-dropdown-button").innerHTML = `${categoryName} <span class="caret"></span>`;
+
+  return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+  HELPER FUNCTIONS
+*/
 
 function getById(id) {
   return document.getElementById(id);
@@ -11,4 +49,22 @@ function scrollToTop() {
 
 function destroyHTML(id) {
   getById(id).innerHTML = new String();
+}
+
+function getFromStorage(key) {
+  return JSON.parse(window.localStorage.getItem(key));
+}
+
+function putInStorage(key, value) {
+  return localStorage.setItem(key, JSON.stringify(value));
+}
+
+function addListenerToClass(className, event, handler) {
+  const elements = document.getElementsByClassName(className);
+
+  Array.from(elements).forEach(element => {
+    element.addEventListener(event, handler);
+  });
+
+  return true;
 }
