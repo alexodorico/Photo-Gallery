7784cb090d11091e3dbd866ee8957cf8e2601aa4
Alex Odorico <alex.odorico@morleynet.com>
2019-06-27 16:23:24 -0400

    Added config option for container width

--- gallery.config.js
+++ gallery.config.js
@@ -3,6 +3,9 @@ const options = {
   // Base for API calls
   endpoint: "https://morleygrouptravel-stg.morleycms.com/widgets/photoGalleryv3/getCategory.ashx?",
 
+  // Need to fix this later add get width using js, then fall back to 900
+  containerWidth: 900,
+
   // How many photos in a row
   rowLength: 3,
 
