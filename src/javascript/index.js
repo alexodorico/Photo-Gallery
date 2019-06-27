2b3aa0c2b7c890d3f437344fa62c2e22ebb2d02a
Alex Odorico <alex.odorico@morleynet.com>
2019-06-27 12:27:56 -0400

    Cleaned up html and rewrote scss

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,2 +1,14 @@
-import './zip';
+// import './zip';
 import '../scss/styles.scss';
+
+function getById(id) {
+  return document.getElementById(id);
+}
+
+function scrollToTop() {
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
+}
+
+function destroyHTML(id) {
+  getById(id).innerHTML = new String();
+}
