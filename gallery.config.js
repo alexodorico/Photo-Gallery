80e3575d8b2de388e1415a7f8cc1590da2c0d3ca
Alex Odorico <alex.odorico@morleynet.com>
2019-06-27 14:50:05 -0400

    Created config file

---
+++ gallery.config.js
@@ -0,0 +1,20 @@
+const options = {
+
+  // Base for API calls
+  endpoint: "https://morleygrouptravel-stg.morleycms.com/widgets/photoGalleryv3/getCategory.ashx?",
+
+  // How many photos in a row
+  rowLength: 3,
+
+  // Sum of photo margin left and margin right
+  photoMargin: 10,
+
+  // Sum of container padding left and padding right
+  containerPadding: 28,
+  
+  // Photos per API call
+  photoLimit: 24
+
+}
+
+export default options;
