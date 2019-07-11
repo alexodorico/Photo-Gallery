daf312c5080f5ce1cdfefa1f2971709aff00d8f7
Alex Odorico <alex.odorico@morleynet.com>
2019-07-11 12:07:36 -0400

    Formatted

--- src/javascript/zip.js
+++ src/javascript/zip.js
@@ -1,65 +1,56 @@
-var total, count, failed;
-var zip = new JSZip();
+let total, count, failed;
+const zip = new JSZip();
 
-		function createZip(urls, zipName)
-		{
-			count = 0, failed = 0;
-			total = urls.length;
+function createZip(urls, zipName) {
+	count = 0, failed = 0;
+	total = urls.length;
+	for (var i in urls) addFile(urls[i], zipName);
+}
 
-			console.log('zip creation started');
+function addFile(path, zipName) {
+	let parts = getFileParts(path);
 
-			for (var i in urls) addFile(urls[i], zipName);
+	JSZipUtils.getBinaryContent(path, function (err, data) {
+		if (err) {
+			failed++;
+		} else {
+			zip.file(parts[0] + '.' + parts[1], data, { binary: true });
+			checkComplete(zipName);
 		}
+	});
+}
 
-		function addFile(path, zipName)
-		{
-			var parts = getFileParts(path);
+function checkComplete(zipName) {
+	count++;
 
-			JSZipUtils.getBinaryContent(path, function (err, data) {
-				if (err) {
-					console.error('JSZIP: Failed to retrieve data. \n' + path);
-					failed++;
-				} else {
-					zip.file(parts[0] + '.' + parts[1], data, { binary: true });
+	if (count + failed == total) {
+		zip.generateAsync({type:"blob"})
+			.then(function(content) {
+					saveAs(content, zipName + '.zip');
 
-					checkComplete(zipName);
-				}
+					// Notify user of errors
+					if (failed != 0) {
+						alert("Whoops!  Looks like we couldn't get all those files you requested.");
+					}
+
+					alert('complete');
 			});
-		}
 
-		function checkComplete(zipName)
-		{
-			count++;
+		// Reset for next download
+		count = 0, failed = 0;
+		zip = new JSZip();
+	}
+}
 
-			if (count + failed == total) {
-				zip.generateAsync({type:"blob"})
-					.then(function(content) {
-					    saveAs(content, zipName + '.zip');
+function getFileParts(path) {
+	var b = path.lastIndexOf('/') + 1;
+	var e = path.indexOf('?');
+	if (e < 0) e = path.length;
+	var filename = path.substr(b, e - b);
 
-					    // Notify user of errors
-					    if (failed != 0) {
-							alert("Whoops!  Looks like we couldn't get all those files you requested.");
-					    }
+	var d = filename.indexOf('.');
+	var fn = filename.substr(0, d);
+	var ext = filename.substr(d + 1, filename.length - d);
 
-					    alert('complete');
-					});
-
-				// Reset for next download
-				count = 0, failed = 0;
-				zip = new JSZip();
-			}
-		}
-
-		function getFileParts(path)
-		{
-				var b = path.lastIndexOf('/') + 1;
-				var e = path.indexOf('?');
-				if (e < 0) e = path.length;
-				var filename = path.substr(b, e - b);
-
-				var d = filename.indexOf('.');
-				var fn = filename.substr(0, d);
-				var ext = filename.substr(d + 1, filename.length - d);
-
-				return [fn, ext];
-		}
+	return [fn, ext];
+}
